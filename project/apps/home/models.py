f11a74c1200104dad8b64f4a08695ea66b88d7ff
Pablo Smole <pablo.smole@gmail.com>
2023-05-28 20:18:27 -0300

    Creación de modelos (Producto, ProductoCategoría y Cliente)

--- project/apps/home/models.py
+++ project/apps/home/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    desripcion = models.TextField()
+
+    def __str__(self):
+        return self.nombre
+    
+class ProductoCategoria(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+    
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    edad = models.IntegerField()
+    
+    def __str__(self):
+        return self.nombre
